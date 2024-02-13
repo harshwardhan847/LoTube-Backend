0096314b6fc2b26e4e86fc111f0bc511e730304f
Harsh Wardhan <harshwardhan847@gmail.com>
2024-02-13 11:43:05 +0530

    Added utils classes and fn for err and response

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT: ${process.env.PORT || 8000}`);
+    });
+    app.on("error", (error) => {
+      console.log(error);
+      throw error;
+    });
+  })
+  .catch((err) => {
+    console.error("Mongo DB connection failed!!!", err);
+  });
